c49b5d7956f44a51d673450d702b3ae02b0f52fc
joao-lucas-paes <65510376+joao-lucas-paes@users.noreply.github.com>
2021-07-07 18:45:29 -0400

    emailSender

--- src/pages.js
+++ src/pages.js
@@ -1,7 +1,16 @@
 const db = require('./db.js');
+const email_sender_node = require('nodemailer');
+const transporter = email_sender_node.createTransport({
+  service:"Gmail",
+  host:"smtp",
+  auth:{
+    user:"desenvolvimentow2angdav@gmail.com",
+    pass:"yxWKnpmW3mi3RbE"
+  }
+})
 
 function saveForm(req, res) {
-	const {nome, email, msg} = req.body; 
+	const {nome, email, msg} = req.body;
 
 	if (
 		(nome == undefined)||
@@ -10,8 +19,21 @@ function saveForm(req, res) {
 	){
 		return res.status(418).json({"ok":false, "error":"Falta um campo ae"})
 	}
+  let emailToSend = {
+    from:"desenvolvimentow2angdav@gmail.com/",
+    to:email,
+    subject:`${name}, olá e bem-vindo! Hello there and `,
+    text:`${nome},\n\nPt-BR: Nós do projeto só podemos agradecer, seu interesse demonstra que estamos indo pelo caminho certo e isso nos deixa muito agradecido, obrigado!\n\n En-USA: we from the project are very thankful, you're interested show to us that we are going to the correct way, thank you! \n\nASS: João Lucas & Êxodo Jaffar.`
+  }
+  email_sender(emailToSend, (err)=>{
+    if(err){
+      console.log(err);
+    }else{
+      console.log("sent");
+    }
+  });
 
-	db.saveDataOfContato({nome, email, msg})	
+	db.saveDataOfContato({nome, email, msg})
 
 	return res.status(200).json({"ok": true})
 }
@@ -55,16 +77,13 @@ function showData(req, res) {
     if (err) throw err;
 
     for (let row of res.rows) {
-      addRowOnTable(row)      
+      addRowOnTable(row)
     }
 
     endComposition()
 
   })
 
-  
-
-  
 }
 
 module.exports = {
