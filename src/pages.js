3dc9bf3e6d0ee692f5b27f5f5b48f9724949d0d8
joao-lucas-paes <65510376+joao-lucas-paes@users.noreply.github.com>
2021-07-08 00:25:48 -0400

    email fixed

--- src/pages.js
+++ src/pages.js
@@ -1,4 +1,25 @@
+require('dotenv').config()
 const db = require('./db.js');
+const email_sender_node = require('nodemailer');
+const transporter = email_sender_node.createTransport({
+  service:"Gmail",
+  host:"smtp",
+  auth:{
+    user:process.env.USERGMAIL,
+    pass:process.env.PASSGMAIL
+  }
+})
+let sendMail = (emailToSend) =>{
+  transporter.sendMail(emailToSend, (err)=>{
+    if(err){
+      console.log(err);
+    }else{
+      console.log("sent");
+    }
+  })
+}
+
+// code zone =====================================================================================================
 
 function saveForm(req, res) {
 	const {nome, email, msg} = req.body; 
@@ -10,8 +31,23 @@ function saveForm(req, res) {
 	){
 		return res.status(418).json({"ok":false, "error":"Falta um campo ae"})
 	}
+  let emailToUser = {
+    from:process.env.USERGMAIL,
+    to:email,
+    subject:`Projeto AngDav aqui/here!`,
+    text:`${nome},\n\nPt-BR: Nós do projeto só podemos agradecer, seu interesse demonstra que estamos indo pelo caminho certo e isso nos deixa muito agradecido, obrigado!\nEn-USA: We from the project are very thankful, you're interested show to us that we are going to the correct way, thank you! \n\nASS: João Lucas & Êxodo Jaffar.`
+  }
+  let emailToUs = {
+    from:process.env.USERGMAIL,
+    to:process.env.USERGMAIL,
+    subject:`from: ${nome}`,
+    text:`${msg}`
+  }
+  sendMail(emailToUser);
+  sendMail(emailToUs);
 
-	db.saveDataOfContato({nome, email, msg})	
+
+	db.saveDataOfContato({nome, email, msg})
 
 	return res.status(200).json({"ok": true})
 }
@@ -62,9 +98,6 @@ function showData(req, res) {
 
   })
 
-  
-
-  
 }
 
 module.exports = {
